dcfff67c0714e1d21709f8678f081f6d2beca941
Andrea D'Amore <anddam@brapi.net>
2012-10-31 18:44:55 +0100

    creating parent directory in export script

--- export_to_vim.sh
+++ export_to_vim.sh
@@ -3,5 +3,5 @@
 TARGET_DIR=javacomplete
 TARGET_BRANCH=mine
 rm -rf "$HOME"/.vim/bundle/"$TARGET_DIR"
-mkdir "$HOME"/.vim/bundle/"$TARGET_DIR"
+mkdir -p "$HOME"/.vim/bundle/"$TARGET_DIR"
 git archive "$TARGET_BRANCH" | tar -x -C "$HOME"/.vim/bundle/"$TARGET_DIR"
